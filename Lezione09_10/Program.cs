df2f84aac1eeb75d9b76d58325f1824feef6f7b3
Marco Tassone <tassonemarco67@gmail.com>
2025-10-09 10:32:25 +0200

    Update Program.cs

--- Lezione09_10/Program.cs
+++ Lezione09_10/Program.cs
@@ -11,37 +11,48 @@ public class Program
 
 
     List<Operatore> operatori = new List<Operatore>();
-    Operatore oe = new OperatoreEmergenza(" ", " ", 0);
     bool continua = true;
-    string scelta = " ";
+    string? scelta = " ";
     while (continua)
     {
       Console.WriteLine($"-----Menu Operatori-----");
       Console.WriteLine($"a. Aggiungi operatore emergenza.");
-      Console.WriteLine($"a. Aggiungi operatore sicurezza.");
-      Console.WriteLine($"a. Aggiungi operatore logistica.");
-      Console.WriteLine($"b. Stampa tutti gli operatori.");
-      Console.WriteLine($"c. Chiamare EseguiCompito() di tutti.");
-      Console.WriteLine($"d. Uscire.");
+      Console.WriteLine($"b. Aggiungi operatore sicurezza.");
+      Console.WriteLine($"c. Aggiungi operatore logistica.");
+      Console.WriteLine($"d. Stampa tutti gli operatori.");
+      Console.WriteLine($"e. Chiamare EseguiCompito() di tutti.");
+      Console.WriteLine($"f. Uscire.");
       scelta = Console.ReadLine();
       switch (scelta)
       {
         case "a":
-          if (oe is OperatoreEmergenza)
-          {
-            Console.WriteLine($"Inserisci il nome dell'operatore: ");
-            string? nome = Console.ReadLine();
-            Console.WriteLine($"Inserisci il turno dell'operatore: ");
-            string? turno = Console.ReadLine();
-            Console.WriteLine($"Livello di emergenza gestito: ");
-            int emergenza = Convert.ToInt32(Console.ReadLine());
-            oe = new OperatoreEmergenza(nome, turno, emergenza);
-            operatori.Add(oe);
-          }
+          Console.WriteLine($"Inserisci nome dell'operatore: ");
+          string? nomeOE = Console.ReadLine();
+          Console.WriteLine($"Inserisci il turno dell'operatore: ");
+          string? turnoOE = Console.ReadLine();
+          Console.WriteLine($"Inserisci livello di emergenza: ");
+          int emergenza = Convert.ToInt32(Console.ReadLine());
+          operatori.Add(new OperatoreEmergenza(nomeOE, turnoOE, emergenza));
           break;
-        case "b": foreach (Operatore o in operatori) o.Stampa(); break;
-        case "c": foreach (Operatore o in operatori) o.EseguiCompito(); break;
-        case "d": continua = false; break;
+        case "b":
+          Console.WriteLine($"Inserisci nome dell'operatore: ");
+          string? nomeOS = Console.ReadLine();
+          Console.WriteLine($"Inserisci il turno dell'operatore: ");
+          string? turnoOS = Console.ReadLine();
+          Console.WriteLine($"Inserisci area sorvegliata dall'operatore: ");
+          string? areaSorvegliata = Console.ReadLine();
+          operatori.Add(new OperatoreSicurezza(nomeOS, turnoOS, areaSorvegliata));
+          break;
+        case "c":
+          Console.WriteLine($"Inserisci nome dell'operatore: ");
+          string? nomeOL = Console.ReadLine();
+          Console.WriteLine($"Inserisci il turno dell'operatore: ");
+          string? turnoOL = Console.ReadLine();
+          Console.WriteLine($"Inserisci il numero consegne dell'operatore: ");
+          int numeroConsegne = Convert.ToInt32(Console.ReadLine());
+          break;
+        case "d":
+          break;
       }
     }
   }
