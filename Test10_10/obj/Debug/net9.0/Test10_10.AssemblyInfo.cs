a0ece88604282e4b7eeb609e18d10c66387d0a25
Marco Tassone <tassonemarco67@gmail.com>
2025-10-27 15:40:32 +0100

    meme

--- Test10_10/obj/Debug/net9.0/Test10_10.AssemblyInfo.cs
+++ Test10_10/obj/Debug/net9.0/Test10_10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test10_10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae3aca975280a3edcf10cd46f7fdb7a27d15c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d8138f3dfff83ca9ee05c1bae147529bb8643e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test10_10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test10_10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
